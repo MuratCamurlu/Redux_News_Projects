60f8a31d0ebde0a7ce77b3fe6e06e2f25eec5181
MuratÇamurlu <mcamurlu1923@gmailcom>
2022-12-25 15:15:43 +0300

    createThunk

--- src/features/newsSlice.jsx
+++ src/features/newsSlice.jsx
@@ -1,10 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   newsList: [],
   loading: false,
   error: false,
 };
+
+export const getNews = createAsyncThunk("getNews", async () => {
+  const API_KEY = "242fb770517b456dac640c348e0fed1c";
+  const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+  try {
+    const { data } = await axios(url);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const newsSlice = createSlice({
   name: "news",
@@ -13,6 +24,23 @@ const newsSlice = createSlice({
     clearNewsList: (state) => {
       state.newsList = [];
     },
+    clearNewsList: (state) => {
+      state.newsList = [];
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getNews.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getNews.fulfilled, (state, { payload }) => {
+        state.newsList = payload;
+        state.loading = false;
+      })
+      .addCase(getNews.rejected, (state) => {
+        state.error = true;
+        state.loading = false;
+      });
   },
 });
 
